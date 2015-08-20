c5a4de801e28aae57ab960dccf3842d5b5e7e871
Ian Buss <ibuss@cloudera.com>
2015-08-20 17:21:32 +0100

    Cope with Parquet files with no footer

--- src/main/java/com/cloudera/hadoop/detectors/ParquetDetector.java
+++ src/main/java/com/cloudera/hadoop/detectors/ParquetDetector.java
@@ -31,24 +31,34 @@ public class ParquetDetector extends AbstractDetector {
 
     @Override
     public FileReport analyze(PathData file, String scanDate) throws IOException {
-        ParquetMetadata meta = ParquetFileReader.readFooter(file.fs.getConf(),
-          file.stat, ParquetMetadataConverter.NO_FILTER);
+        ParquetMetadata meta = null;
+        try {
+            meta = ParquetFileReader.readFooter(file.fs.getConf(),
+              file.stat, ParquetMetadataConverter.NO_FILTER);
+        } catch (RuntimeException e) {
+            // Some Parquet files seem to just contain 'PAR1', catch and
+            // do something more intelligent, probably an 'PARQ_EMPTY' advisory
+            // TODO: add logging
+        }
 
         // This is a bit naff but just check for any compression
         // in any column in the first block for now
-        CompressionType compressionType = CompressionType.NONE;
-        if (meta.getBlocks().size() > 0) {
-            for (ColumnChunkMetaData metaData : meta.getBlocks().get(0).getColumns()) {
-                CompressionCodecName codecName = metaData.getCodec();
-                if (codecName.equals(CompressionCodecName.GZIP)) {
-                    compressionType = CompressionType.GZIP;
-                    break;
-                } else if (codecName.equals(CompressionCodecName.LZO)) {
-                    compressionType = CompressionType.LZO;
-                    break;
-                } else if (codecName.equals(CompressionCodecName.SNAPPY)) {
-                    compressionType = CompressionType.SNAPPY;
-                    break;
+        CompressionType compressionType = CompressionType.UNKNOWN;
+        if (meta != null) {
+            compressionType = CompressionType.NONE;
+            if (meta.getBlocks().size() > 0) {
+                for (ColumnChunkMetaData metaData : meta.getBlocks().get(0).getColumns()) {
+                    CompressionCodecName codecName = metaData.getCodec();
+                    if (codecName.equals(CompressionCodecName.GZIP)) {
+                        compressionType = CompressionType.GZIP;
+                        break;
+                    } else if (codecName.equals(CompressionCodecName.LZO)) {
+                        compressionType = CompressionType.LZO;
+                        break;
+                    } else if (codecName.equals(CompressionCodecName.SNAPPY)) {
+                        compressionType = CompressionType.SNAPPY;
+                        break;
+                    }
                 }
             }
         }
